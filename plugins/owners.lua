66e95a90640b0af0fd2f96b86ce2e3fbf15bd1d0
Max-pbot <poriakarimi10@gmail.com>
2016-02-22 07:58:51 -0800

    Update owners.lua

--- plugins/owners.lua
+++ plugins/owners.lua
@@ -4,12 +4,12 @@ local function lock_group_namemod(msg, data, target)
   local group_name_set = data[tostring(target)]['settings']['set_name']
   local group_name_lock = data[tostring(target)]['settings']['lock_name']
   if group_name_lock == 'yes' then
-    return 'Group name is already locked'
+    return 'User ['..user_id..' ]esme gorooh ghofl ast!'
   else
     data[tostring(target)]['settings']['lock_name'] = 'yes'
     save_data(_config.moderation.data, data)
     rename_chat('chat#id'..target, group_name_set, ok_cb, false)
-  return 'Group name has been locked'
+  return 'User ['..user_id..' ]esme gorooh ghofl shod!'
   end
 end
 
@@ -17,66 +17,66 @@ local function unlock_group_namemod(msg, data, target)
   local group_name_set = data[tostring(target)]['settings']['set_name']
   local group_name_lock = data[tostring(target)]['settings']['lock_name']
   if group_name_lock == 'no' then
-    return 'Group name is already unlocked'
+    return 'User ['..user_id..' ]esme gorooh baz ast!'
   else
     data[tostring(target)]['settings']['lock_name'] = 'no'
     save_data(_config.moderation.data, data)
-  return 'Group name has been unlocked'
+  return 'User ['..user_id..' ]esme gorooh baz shod!'
   end
 end
 
 local function lock_group_floodmod(msg, data, target)
   local group_flood_lock = data[tostring(target)]['settings']['flood']
   if group_flood_lock == 'yes' then
-    return 'Group flood is locked'
+    return 'User ['..user_id..' ]floode gorooh ghofl ast!'
   else
     data[tostring(target)]['settings']['flood'] = 'yes'
     save_data(_config.moderation.data, data)
-  return 'Group flood has been locked'
+  return 'User ['..user_id..' ]floode gorooh ghofl shod!'
   end
 end
 
 local function unlock_group_floodmod(msg, data, target)
   local group_flood_lock = data[tostring(target)]['settings']['flood']
   if group_flood_lock == 'no' then
-    return 'Group flood is not locked'
+    return 'User ['..user_id..' ]floode gorooh baz ast!'
   else
     data[tostring(target)]['settings']['flood'] = 'no'
     save_data(_config.moderation.data, data)
-  return 'Group flood has been unlocked'
+  return 'User ['..user_id..' ]floode gorooh baz shod!'
   end
 end
 
 local function lock_group_membermod(msg, data, target)
   local group_member_lock = data[tostring(target)]['settings']['lock_member']
   if group_member_lock == 'yes' then
-    return 'Group members are already locked'
+    return 'User ['..user_id..' ]addmembere gorooh ghofl ast!'
   else
     data[tostring(target)]['settings']['lock_member'] = 'yes'
     save_data(_config.moderation.data, data)
   end
-  return 'Group members has been locked'
+  return 'User ['..user_id..' ]addmembere gorooh ghofl shod!'
 end
 
 local function unlock_group_membermod(msg, data, target)
   local group_member_lock = data[tostring(target)]['settings']['lock_member']
   if group_member_lock == 'no' then
-    return 'Group members are not locked'
+    return 'User ['..user_id..' ]addmembere gorooh baz ast!'
   else
     data[tostring(target)]['settings']['lock_member'] = 'no'
     save_data(_config.moderation.data, data)
-  return 'Group members has been unlocked'
+  return 'User ['..user_id..' ]addmembere gorooh baz shod!'
   end
 end
 
 local function unlock_group_photomod(msg, data, target)
   local group_photo_lock = data[tostring(target)]['settings']['lock_photo']
   if group_photo_lock == 'no' then
-      return 'Group photo is not locked'
+      return 'User ['..user_id..' ]axe gorooh baz ast!'
   else
       data[tostring(target)]['settings']['lock_photo'] = 'no'
       save_data(_config.moderation.data, data)
-  return 'Group photo has been unlocked'
+  return 'User ['..user_id..' ]axe gorooh baz shod!'
   end
 end
 
@@ -100,7 +100,7 @@ local function set_rules(target, rules)
   local data_cat = 'rules'
   data[tostring(target)][data_cat] = rules
   save_data(_config.moderation.data, data)
-  return 'Set group rules to:\n'..rules
+  return 'User ['..user_id..' ]taghire ghavanine gorooh be:\n'..rules
 end
 
 local function set_description(target, about)
@@ -108,7 +108,7 @@ local function set_description(target, about)
   local data_cat = 'description'
   data[tostring(target)][data_cat] = about
   save_data(_config.moderation.data, data)
-  return 'Set group description to:\n'..about
+  return 'User ['..user_id..' ]taghire etelaate gorooh be:\n'..about
 end
 
 local function run(msg, matches)
@@ -119,7 +119,7 @@ local function run(msg, matches)
     if matches[2] == 'ban' then
       local chat_id = matches[1]
       if not is_owner2(msg.from.id, chat_id) then
-        return "You are not the owner of this group"
+        return "User ["..user_id.." ]shoma modire in gorooh nistid!"
       end
       if tonumber(matches[3]) == tonumber(our_id) then return false end
       local user_id = matches[3]
@@ -128,55 +128,55 @@ local function run(msg, matches)
       end
       ban_user(matches[3], matches[1])
       local name = user_print_name(msg.from)
-      savelog(matches[1], name.." ["..msg.from.id.."] banned user ".. matches[3])
-      return 'User '..user_id..' banned'
+      savelog(matches[1], name.." ["..msg.from.id.."]User ["..user_id.." ] User ban shode ast! ".. matches[3])
+      return 'User '..user_id..' ban shode ast'
     end
     if matches[2] == 'unban' then
     if tonumber(matches[3]) == tonumber(our_id) then return false end
       local chat_id = matches[1]
       if not is_owner2(msg.from.id, chat_id) then
-        return "You are not the owner of this group"
+        return "User ["..user_id.." ]shoma modire in gorooh nistid!"
       end
       local user_id = matches[3]
       if tonumber(matches[3]) == tonumber(msg.from.id) then 
-        return "You can't unban yourself"
+        return "User ["..user_id.." ]shoma nmitoonid khodetono unban konid!"
       end
       local hash =  'banned:'..matches[1]
       redis:srem(hash, user_id)
       local name = user_print_name(msg.from)
-      savelog(matches[1], name.." ["..msg.from.id.."] unbanned user ".. matches[3])
-      return 'User '..user_id..' unbanned'
+      savelog(matches[1], name.." ["..msg.from.id.."]User ["..user_id.." ] User unban shode ast! ".. matches[3])
+      return 'User '..user_id..' unban shod!'
     end
     if matches[2] == 'kick' then
       local chat_id = matches[1]
       if not is_owner2(msg.from.id, chat_id) then
-        return "You are not the owner of this group"
+        return "User ["..user_id.." ]shoma modire in gorooh nistid!"
       end
       if tonumber(matches[3]) == tonumber(our_id) then return false end
       local user_id = matches[3]
       if tonumber(matches[3]) == tonumber(msg.from.id) then 
-        return "You can't kick yourself"
+        return "User ["..user_id.." ]shoma nmitoonid khodetono kick konid!"
       end
       kick_user(matches[3], matches[1])
       local name = user_print_name(msg.from)
-      savelog(matches[1], name.." ["..msg.from.id.."] kicked user ".. matches[3])
-      return 'User '..user_id..' kicked'
+      savelog(matches[1], name.." ["..msg.from.id.."]User ["..user_id.." ] User kick shod! ".. matches[3])
+      return 'User '..user_id..' kick shod!'
     end
     if matches[2] == 'clean' then
       if matches[3] == 'modlist' then
         if not is_owner2(msg.from.id, chat_id) then
-          return "You are not the owner of this group"
+          return "User ["..user_id.." ]shoma modire in gorooh nistid!"
         end
         for k,v in pairs(data[tostring(matches[1])]['moderators']) do
           data[tostring(matches[1])]['moderators'][tostring(k)] = nil
           save_data(_config.moderation.data, data)
         end
         local name = user_print_name(msg.from)
-        savelog(matches[1], name.." ["..msg.from.id.."] cleaned modlist")
+        savelog(matches[1], name.." ["..msg.from.id.."]User ["..user_id.." ] Mod list ra pak kard!")
       end
       if matches[3] == 'rules' then
         if not is_owner2(msg.from.id, chat_id) then
-          return "You are not the owner of this group"
+          return "User ["..user_id.." ]shoma modire in gorooh nistid! "
         end
         local data_cat = 'rules'
         data[tostring(matches[1])][data_cat] = nil
@@ -186,65 +186,65 @@ local function run(msg, matches)
       end
       if matches[3] == 'about' then
         if not is_owner2(msg.from.id, chat_id) then
-          return "You are not the owner of this group"
+          return "User ["..user_id.." ]shoma modire in gorooh nistid!"
         end
         local data_cat = 'description'
         data[tostring(matches[1])][data_cat] = nil
         save_data(_config.moderation.data, data)
         local name = user_print_name(msg.from)
-        savelog(matches[1], name.." ["..msg.from.id.."] cleaned about")
+        savelog(matches[1], name.." ["..msg.from.id.."]User ["..user_id.." ] About gorooh ra pak kard")
       end
     end
     if matches[2] == "setflood" then
       if not is_owner2(msg.from.id, chat_id) then
-        return "You are not the owner of this group"
+        return "User ["..user_id.." ]shoma modire in gorooh nistid!"
       end
       if tonumber(matches[3]) < 5 or tonumber(matches[3]) > 20 then
-        return "Wrong number,range is [5-20]"
+        return "User ["..user_id.." ] Khata!Lotfan adade morede nazaretono beyne adade [5ta20]entekhab konid!"
       end
       local flood_max = matches[3]
       data[tostring(matches[1])]['settings']['flood_msg_max'] = flood_max
       save_data(_config.moderation.data, data)
       local name = user_print_name(msg.from)
-      savelog(matches[1], name.." ["..msg.from.id.."] set flood to ["..matches[3].."]")
-      return 'Group flood has been set to '..matches[3]
+      savelog(matches[1], name.." ["..msg.from.id.."]User ["..user_id.." ] set flood be ["..matches[3].."]")
+      return 'User ['..user_id..' ]floode gorooh set shavad be '..matches[3]
     end
     if matches[2] == 'lock' then
       if not is_owner2(msg.from.id, chat_id) then
-        return "You are not the owner of this group"
+        return "User ["..user_id.." ]shoma modire in gorooh nistid!"
       end
       local target = matches[1]
       if matches[3] == 'name' then
         local name = user_print_name(msg.from)
-        savelog(matches[1], name.." ["..msg.from.id.."] locked name ")
+        savelog(matches[1], name.." ["..msg.from.id.."]User ["..user_id.." ] Esm ghofl shode ast! ")
         return lock_group_namemod(msg, data, target)
       end
       if matches[3] == 'member' then
         local name = user_print_name(msg.from)
-        savelog(matches[1], name.." ["..msg.from.id.."] locked member ")
+        savelog(matches[1], name.." ["..msg.from.id.."]User ["..user_id.." ] Member ghofl shode ast! ")
         return lock_group_membermod(msg, data, target)
       end
     end
     if matches[2] == 'unlock' then
       if not is_owner2(msg.from.id, chat_id) then
-        return "You are not the owner of this group"
+        return "User ["..user_id.." ]shoma modire in gorooh nistid!"
       end
       local target = matches[1]
       if matches[3] == 'name' then
         local name = user_print_name(msg.from)
-        savelog(matches[1], name.." ["..msg.from.id.."] unlocked name ")
+        savelog(matches[1], name.." ["..msg.from.id.."]User ["..user_id.." ] Esme gorooh baz shode ast! ")
         return unlock_group_namemod(msg, data, target)
       end
       if matches[3] == 'member' then
         local name = user_print_name(msg.from)
-        savelog(matches[1], name.." ["..msg.from.id.."] unlocked member ")
+        savelog(matches[1], name.." ["..msg.from.id.."]User ["..user_id.." ] Member baz shode ast! ")
         return unlock_group_membermod(msg, data, target)
       end
     end
     if matches[2] == 'new' then
       if matches[3] == 'link' then
         if not is_owner2(msg.from.id, chat_id) then
-          return "You are not the owner of this group"
+          return "User ["..user_id.." ]shoma modire in gorooh nistid!"
         end
         local function callback (extra , success, result)
           local receiver = 'chat#'..matches[1]
@@ -257,17 +257,17 @@ local function run(msg, matches)
         local name = user_print_name(msg.from)
         savelog(matches[1], name.." ["..msg.from.id.."] revoked group link ")
         export_chat_link(receiver, callback, true)
-        return "Created a new new link ! \n owner can get it by /owners "..matches[1].." get link"
+        return "User ["..user_id.." ]linke jadid sakhte shod! ! \n owner can get it by /owners "..matches[1].." get link"
       end
     end
     if matches[2] == 'get' then 
       if matches[3] == 'link' then
         if not is_owner2(msg.from.id, chat_id) then
-          return "You are not the owner of this group"
+          return "User ["..user_id.." ]shoma modire in gorooh nistid!"
         end
         local group_link = data[tostring(matches[1])]['settings']['set_link']
         if not group_link then 
-          return "Create a link using /newlink first !"
+          return "User ["..user_id.." ]baraye sakhte link /newlink ra type konid! first !"
         end
         local name = user_print_name(msg.from)
         savelog(matches[1], name.." ["..msg.from.id.."] requested group link ["..group_link.."]")
@@ -278,14 +278,14 @@ local function run(msg, matches)
       local target = matches[2]
       local about = matches[3]
       local name = user_print_name(msg.from)
-      savelog(matches[2], name.." ["..msg.from.id.."] has changed group description to ["..matches[3].."]")
+      savelog(matches[2], name.." ["..msg.from.id.."]User ["..user_id.." ] etelaate gorooh ra taghir dad be ["..matches[3].."]")
       return set_description(target, about)
     end
     if matches[1] == 'changerules' and is_owner2(msg.from.id, matches[2]) then
       local rules = matches[3]
       local target = matches[2]
       local name = user_print_name(msg.from)
-      savelog(matches[2], name.." ["..msg.from.id.."] has changed group rules to ["..matches[3].."]")
+      savelog(matches[2], name.." ["..msg.from.id.."]User ["..user_id.." ] Ghavanine goroh ro taghir dad be ["..matches[3].."]")
       return set_rules(target, rules)
     end
     if matches[1] == 'changename' and is_owner2(msg.from.id, matches[2]) then
